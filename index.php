db543da4b725579f07edc14cdf9151b0acd2850c
Kyle Polich <kylepolich@gmail.com>
2015-04-14 07:45:31 -0700

    outline

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <html>
 <body>
+<h1>Data Science Podcasts</h1>
 <?php
-	$files = scandir("rss");
-	print_r($files);
+	$files = scandir("feeds");
+	foreach ($files as $file) {
+		$l = strlen($file);
+		$ext = substr($file, $l-4, $l);
+		if ($ext == ".rss") {
+			echo("<h2>" . $file . "</h2>");
+			// TODO: open XML file, parse it
+			// TODO: extract title, image, other metadata
+			// TODO: link to a dynamic subpage that displays all the episodes
+		}
+	}
 ?>
+<hr/>
+(c) 2016
 </body>
 </html>
