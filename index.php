98c240106816808201bdd27f2128bb8ff3bd125b
sohailit <sohailit@gmail.com>
2015-04-15 02:14:41 +0500

    Update index.php
    Added code for parsing XML file

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 		$ext = substr($file, $l-4, $l);
 		if ($ext == ".rss") {
 			echo("<h2>" . $file . "</h2>");
+	$xml=simplexml_load_file("data-skeptic.rss") or die("Error: Cannot create object"); //XML file loaded for parsing
+	echo $xml->channel->title . "<br>";  //Parsed and get title
+	echo "<img src=" . $xml->channel->image->url ." height=200 width=250>"; // 
 			// TODO: open XML file, parse it
 			// TODO: extract title, image, other metadata
 			// TODO: link to a dynamic subpage that displays all the episodes
