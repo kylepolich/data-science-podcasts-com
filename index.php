d4c8ec2727ab3bb666d217f19dffb130d663536c
Sohail Tariq <sohailit@gmail.com>
2015-04-16 22:50:29 +0500

    Update index.php
    Links of sub pages are extracted with PHP ...

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 	$xml=simplexml_load_file("data-skeptic.rss") or die("Error: Cannot create object"); //XML file loaded for parsing
 	echo $xml->channel->title . "<br>";  //Parsed and get title
 	echo "<img src=" . $xml->channel->image->url ." height=200 width=250>"; // 
+	foreach($xml->channel->children() as $meta) {
+	echo $meta->link . "<br>";
+	 
 			// TODO: open XML file, parse it
 			// TODO: extract title, image, other metadata
 			// TODO: link to a dynamic subpage that displays all the episodes
