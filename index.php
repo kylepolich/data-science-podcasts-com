91af80d62e0101af9576498a9962a8e41b38a1c9
Kyle Polich <kylepolich@gmail.com>
2015-04-17 02:03:38 -0700

    updated

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	echo "<img src=" . $xml->channel->image->url ." height=200 width=250>"; // 
 	foreach($xml->channel->children() as $meta) {
 	echo $meta->link . "<br>";
-	 
+	 }
 			// TODO: open XML file, parse it
 			// TODO: extract title, image, other metadata
 			// TODO: link to a dynamic subpage that displays all the episodes
