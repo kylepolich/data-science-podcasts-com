ff32661cd8099f4f0e4d18c5fd23076890b39b6f
Sohail Tariq <sohailit@gmail.com>
2015-04-21 21:54:51 +0500

    Update index.php

--- index.php
+++ index.php
@@ -10,15 +10,13 @@
 		$l = strlen($file);
 		$ext = substr($file, $l-4, $l);
 		if ($ext == ".rss") {
-			// TODO: change to title of podcast
+			// TODO: change to title of podcast  ... Done
 			$path="./feeds/" . $file;
 			$xml=simplexml_load_file($path) or die("Error: Cannot create object"); 
 			echo "<h1>" . $xml->channel->title . "</h1>"; 
 			echo("<h2>" . $file . "</h2>");
 			// TODO: put a pretty, responsive output of the metadata
 		
-			//$xml=simplexml_load_file($path) or die("Error: Cannot create object"); //XML file loaded for parsing
-			 //Parsed and get title
 			echo "<img src=" . $xml->channel->image->url ." height=200 width=250>";
 			// TODO: extract title, image, other metadata
 			foreach($xml->channel->children() as $meta) {
