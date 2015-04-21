5e4443a24bd5082787420519c946d011490e392e
Kyle Polich <kylepolich@gmail.com>
2015-04-20 19:19:43 -0700

    adding instructions

--- index.php
+++ index.php
@@ -7,16 +7,18 @@
 		$l = strlen($file);
 		$ext = substr($file, $l-4, $l);
 		if ($ext == ".rss") {
+			// TODO: change to title of podcast
 			echo("<h2>" . $file . "</h2>");
+			// TODO: put a pretty, responsive output of the metadata
 			$path="./feeds/" . $file;
 			$xml=simplexml_load_file($path) or die("Error: Cannot create object"); //XML file loaded for parsing
 			echo $xml->channel->title . "<br>";  //Parsed and get title
 			echo "<img src=" . $xml->channel->image->url ." height=200 width=250>";
+			// TODO: extract title, image, other metadata
 			foreach($xml->channel->children() as $meta) {
 				echo $meta->link . "<br>";
+				// TODO: list only the most recent 3 episodes
 			}
-			// TODO: open XML file, parse it
-			// TODO: extract title, image, other metadata
 			// TODO: link to a dynamic subpage that displays all the episodes
 		}
 	}
