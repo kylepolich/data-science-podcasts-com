95069bdb6ede4e4e29639077c688fa9da58955b9
Kyle Polich <kylepolich@gmail.com>
2015-04-20 19:16:53 -0700

    updates

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 		$ext = substr($file, $l-4, $l);
 		if ($ext == ".rss") {
 			echo("<h2>" . $file . "</h2>");
-        $path="./feeds/data-skeptic.rss";
-	$xml=simplexml_load_file($path) or die("Error: Cannot create object"); //XML file loaded for parsing
-	echo $xml->channel->title . "<br>";  //Parsed and get title
-	echo "<img src=" . $xml->channel->image->url ." height=200 width=250>"; // 
-	foreach($xml->channel->children() as $meta) {
-	echo $meta->link . "<br>";
-	 }
+			$path="./feeds/" . $file;
+			$xml=simplexml_load_file($path) or die("Error: Cannot create object"); //XML file loaded for parsing
+			echo $xml->channel->title . "<br>";  //Parsed and get title
+			echo "<img src=" . $xml->channel->image->url ." height=200 width=250>";
+			foreach($xml->channel->children() as $meta) {
+				echo $meta->link . "<br>";
+			}
 			// TODO: open XML file, parse it
 			// TODO: extract title, image, other metadata
 			// TODO: link to a dynamic subpage that displays all the episodes
