b5444fdffd087aa5a526b0a8184e86aeeb71f290
Sohail Tariq <sohailit@gmail.com>
2015-05-24 00:31:57 +0500

    Update details.php

--- details.php
+++ details.php
@@ -20,42 +20,71 @@ $file=$_GET["p"];
 			$path="./feeds/" . $file;
 			$xml=simplexml_load_file($path) or die("Error: Cannot create object"); 
 			?>
-			<div class="container">
-            	<div class="row">
+					<div class="container">
+            	<div class="row main-row">
 			<?php
-			echo "<h1 class=\"chanel-title\"><a href=details.php?p=$file>" .  $xml->channel->title . "</a></h1>"; 
+			//echo "<h1 class=\"chanel-title\"><a href=details.php?p=$file>" .  $xml->channel->title . "</a></h1>"; 
 			//echo("<h2 class=\"file-nam\">". $file . "</h2>");
 			// TODO: put a pretty, responsive output of the metadata
 		
-			echo "<img src=" . $xml->channel->image->url ." class=\"imgz img-responsive\" height=200 width=250>";
+			//echo "<img src=" . $xml->channel->image->url ." class=\"imgz img-responsive\" height=200 width=250>";
 			// TODO: extract title, image, other metadata
+				?>
+                	<div class="row">
+                    	<div class="col-sm-3">
+                        	<?php
+                            	echo "<img src=" . $xml->channel->image->url ." class=\"imgz img-responsive\" height=200 width=250>";
+							?>
+                        </div><!-- col-sm-4 end here -->
+                        <div class="col-sm-9">
+                        	<?php
+                            	echo "<h1 class=\"chanel-title\"><a href=details.php?p=$file>" .  $xml->channel->title . "</a></h1>";
+								echo "<p class=\"chanel-detail\" style=\"padding-left:36px;\">".date('n/j/Y H:i:s',strtotime($xml->channel->lastBuildDate))."</p>";
+								echo "<p class=\"chanel-detail\" style=\"padding-left:36px;\">".$xml->channel->description."</p>";
+							?>
+                        </div><!-- col-sm-8 end here -->
+                    </div>
+				<?php
 			
+			$count = 0 ;
 			foreach($xml->channel->children() as $meta) {
 				//echo $meta->link . "<br>";
-				
-				if(empty($meta))
+				if(empty($meta) )
 				{
-					echo"";
+					echo "";
 				}
 				else
 				{
-					?>
-				
-					<div class="row inner-row">
-                    	<div class="col-sm-6"><?php echo "<p class=\"meta-link\" style=\"padding:5px;\">$meta->title </p>"  ; ?></div>
-                        <div class="col-sm-6"><?php echo "<p style=\"padding:5px;\" class=\"meta-link\">$meta->pubDate</p>"  ; ?></div>
-                        <div class="col-sm-12"><?php echo "<p style=\"padding:5px;\" class=\"meta-link\"><a href=\"$meta->link\" >$meta->link</a></p>"  ; ?></div>
-                    </div><!-- inner row  -->
-				<?php
+					if($count < 4)
+					{
+						if($count == 0)
+							{
+								echo"";
+							}
+							else
+							{
+								?>
+					
+						<div class="row inner-row">
+							<div class="col-sm-6"><?php echo "<p class=\"meta-link\" style=\"padding:5px;\">$meta->title </p>"  ; ?></div>
+							<div class="col-sm-6"><?php echo "<p style=\"padding:5px;\" class=\"meta-link\">".date('n/j/Y H:i:s',strtotime($meta->pubDate))."</p>"  ; ?></div>
+							
+                            <div class="col-sm-12"><?php echo substr($meta->description,0,100); ?></div>
+                            
+						</div><!-- inner row  -->
+							<?php
+                            }
+				$count++;
 				//echo "<p class=\"meta-link\"><a href=\"$meta->link\" >$meta->link</a></p>"  ;
-				// TODO: list only the most recent 3 episodes	
-				}
+				// TODO: list only the most recent 3 episodes
+					}			
 				
+				}
 		}
 			?>
             	</div><!-- row end here -->
 			</div>	<!-- container end here -->
-			<?php	
+	<?php	
 			// TODO: link to a dynamic subpage that displays all the episodes
 		}
 	
